3126d094c0b9445ba2715e459fb3d91522673db6
Issa <issa2018m@yandex.com>
2018-11-27 10:34:29 +0600

    Add RichTextEditor component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,74 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import styled from 'react-emotion'
+import './App.css'
+
+import RichTextEditor from './TextEditor/RichTextEditor'
+
+const Wrapper = styled('div')`
+  max-width: 42em;
+  margin: 0 auto 20px;
+  padding: 20px;
+`
+
+const Editor = styled(Wrapper)`
+  background: #fff;
+`
+
+const Warning = styled(Wrapper)`
+  background: #fffae0;
+
+  & > pre {
+    background: #fbf1bd;
+    white-space: pre;
+    overflow-x: scroll;
+    margin-bottom: 0;
+  }
+`
 
 class App extends Component {
-  render() {
+  state = {
+    error: null,
+    info: null,
+  }
+
+  /**
+   * Catch the `error` and `info`.
+   *
+   * @param {Error} error
+   * @param {Object} info
+   */
+
+  componentDidCatch (error, info) {
+    this.setState({error, info})
+  }
+
+  render () {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Rich Text editor</h1>
+          {this.state.error ? (
+            <Warning>
+              <p>
+                An error was thrown by one of the example's React components!
+              </p>
+              <pre>
+                <code>
+                  {this.state.error.stack}
+                  {'\n'}
+                  {this.state.info.componentStack}
+                </code>
+              </pre>
+            </Warning>
+          ) : (
+            <Editor>
+              <RichTextEditor/>
+            </Editor>
+          )}
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
